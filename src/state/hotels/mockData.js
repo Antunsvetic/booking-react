1a8860c7c349823419444a6856b21f46cfae75fb
Antun <antunsvetic23@gmail.com>
2024-03-18 22:11:31 +0100

    update mockData with more hotels

--- src/state/hotels/mockData.js
+++ src/state/hotels/mockData.js
@@ -1,44 +1,27 @@
 export const MOCK_DATA = [
     {
-        id: 1,
+        id: 0,
         name: "Hotel Ambasador",
         bookings: []
     },
     {
-        id: 2,
+        id: 1,
         name: "Hotel Hilton",
-        bookings: [
-            {
-                id: 1,
-                checkIn: "20/03/2024",
-                checkOut: "25/03/2024"
-            },
-            {
-                id: 2,
-                checkIn: "27/03/2024",
-                checkOut: "01/04/2024"
-            }
-        ]
+        bookings: []
+    },
+    {
+        id: 3,
+        name: "Vila Diamond",
+        bookings: []
     },
     {
         id: 3,
         name: "Hotel Adeo Porec",
-        bookings: [
-            {
-                id: 1,
-                checkIn: "20/03/2024",
-                checkOut: "25/03/2024"
-            },
-            {
-                id: 2,
-                checkIn: "27/03/2024",
-                checkOut: "01/04/2024"
-            },
-            {
-                id: 3,
-                checkIn: "03/04/2024",
-                checkOut: "09/04/2024"
-            }
-        ]
+        bookings: []
+    },
+    {
+        id: 4,
+        name: "Apartment Adria",
+        bookings: []
     }
 ]
